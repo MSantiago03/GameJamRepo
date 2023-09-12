f938c6d8e53e0bcbbf712d6f8d25148005f7d62c
Talissa Nam <talissa.nam@duke.edu>
2023-09-12 11:28:17 +0200

    added scripts to rock prefabs and cliffs

---
+++ Unity_Project_Test/Assets/Scripts12/BlueRock.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueRock : Rock
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        level = 2;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
